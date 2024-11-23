e45b1bdec7e52ddad270731e7c6ccb8f37d215c5
yyavv <y.selim.celik.2003@hotmail.com>
2024-11-24 00:47:15 +0300

    refactored main code to adapt new features

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from assets.message import Message
+from assets.commit_tracker import CommitTracker
 
 def main():
     msg = input("Press Enter to your Message: ")
     
     message = Message(msg)
 
-    message.animate_message_by_row(delay=0.1)
+    message.animate_message(delay=0.1)
     
+    commit_tracker = CommitTracker(message)
+
+    commit_tracker.calculateDays()
+
     message.print_stats()
 
 if __name__ == "__main__":
