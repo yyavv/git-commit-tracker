ff4bd6ae5c20084540f0e88ddc28d1a63be23f29
yyavv <y.selim.celik.2003@hotmail.com>
2024-11-24 00:43:12 +0300

    unnecessary directory deleted

--- src/commit_tracker.py
+++
@@ -1,7 +0,0 @@
-class CommitTracker:
-    def __init__(self,message):
-        self._message = message
-
-    def calculateDays(self):
-        print("Calculating days for commit tracker")
-        
